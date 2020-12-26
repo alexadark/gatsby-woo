4535ea9d06e8c8827c66ef7fbd5d6e8fde669466
Alexandra Spalato <alexaspalato@gmail.com>
2020-12-26 16:31:06 +0100

    product

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header className="mb-6 bg-pink-500">
     <div
       style={{
         margin: `0 auto`,
--- src/templates/product.js
+++ src/templates/product.js
@@ -1,13 +1,34 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import { Button } from "@chakra-ui/react"
 
 const Product = ({ data, pageContext }) => {
   const product = data.wpProduct
   console.log("product", product, pageContext)
+  const {
+    name,
+    description,
+    externalUrl,
+    featured,
+    galleryImages,
+    id,
+    image,
+    link,
+    nodeType,
+    price,
+    productCategories,
+    shortDescription,
+    sku,
+    slug,
+    attributes,
+    averageRating,
+  } = product
+
   return (
     <Layout>
-      <h1>Product</h1>
+      <h1>{name}</h1>
+      <Button>{price}</Button>
     </Layout>
   )
 }
